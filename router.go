f0a3c98959bc756b75beb8288eb6db02f73f4e20
bonchondev <benzay212@gmail.com>
2024-10-26 00:56:07 -0400

    updated to clean up a bit

---
+++ router.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	inertia "github.com/romsar/gonertia"
+	"net/http"
+	"log"
+)
+
+func homeHandler(i *inertia.Inertia) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		err := i.Render(w, r, "Home/Index", inertia.Props{
+			"text": "Inertia.js with Svelte and Go! 💙",
+		})
+		if err != nil {
+			handleServerErr(w, err)
+			return
+		}
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func handleServerErr(w http.ResponseWriter, err error) {
+	log.Printf("http error: %s\n", err)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("server error"))
+}
